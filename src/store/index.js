2afb1a83c8893630c2f4f19f3b729bfcd59fd10d
Mohamed Hossam <m.hossameldine@gmail.com>
2022-03-24 14:39:16 +0200

    feat: add cart reducer to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import uiReducer from './reducers/ui-slice';
-
+import cartReducer from './reducers/cart-slice';
 const store = configureStore({
   reducer: {
     ui: uiReducer,
+    cart: cartReducer,
   },
 });
 
